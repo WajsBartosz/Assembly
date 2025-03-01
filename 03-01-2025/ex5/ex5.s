197436e5de900eaa3ef7319427f4c7b3845a9f91
hohland <bartezzwajs@gmail.com>
2025-03-01 15:00:16 +0100

    fixed bug in one file

--- 03-01-2025/ex5/ex5.s
+++ 03-01-2025/ex5/ex5.s
@@ -15,20 +15,3 @@ _start:
     movl $1, %eax
     xorl %ebx, %ebx
     int $0x80
-.section .text
-.global _start
-
-_start:
-    movl $3, %ebx
-    movl $4, %ecx
-    movl $1, %eax
-    movl %ebx, %edx
-    dowhile:
-        imull %ebx, %edx
-        addl $1, %eax 
-        cmp %ecx, %eax
-        js dowhile
-
-    movl $1, %eax
-    xorl %ebx, %ebx
-    int $0x80
